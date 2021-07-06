f196c494aa3bef1ce3695e483625bf2fc573f65e
adargui <adargui@gmail.com>
2021-07-06 16:01:58 +0200

    Ajout du fichier .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+// Connexion à la base de donnee -- https://github.com/fastify/fastify-mongodb
+fastify.register(require('fastify-mongodb'), {
+  // force to close the mongodb connection when app stopped
+  // the default value is false
+  forceClose: true,
+  
+  // url: 'mongodb://mongo/mydb'
+  url: 'mongodb://localhost:27017/superheroes'
+})
 
 // Declare a route
 fastify.get('/', (request, reply) => {
@@ -8,8 +17,32 @@ fastify.get('/', (request, reply) => {
 
 // Déclarer la route /heroes - cette route retournera la liste des avengers
 const avengers = ["Iron man", "Captain america", "Spiderman"]
+
+// heroes GET - Obtiens la liste des héros
 fastify.get('/heroes', ()=>{
     return avengers //equivalent à avengers: avengers
+})
+
+// heroes POST Ajoute un nouvel héro
+fastify.post('/heroes', (request, reply) => {
+  console.log(request.body)
+  const db = fastify.mongo.db
+  const collection = db.collection('heroes')
+  // console.log(collection);
+  // db.collection('inserts')
+  // Insert a single document
+
+
+  // collection.insertOne({
+  //   name: request.body.name,
+  //   powerstats: request.body.powerstats,
+  // })
+  collection.insertOne(request.body)
+
+
+  return null
+  // reply.send(null)
+  
 })
 
 fastify.get('/me', (request, reply) => {
@@ -18,7 +51,7 @@ fastify.get('/me', (request, reply) => {
     return { 
         prenom: 'Abdallah',
         nom: "Dargui d",
-        job: "developpeur web",
+        job: "developpeur web 3",
      }
   })
 
